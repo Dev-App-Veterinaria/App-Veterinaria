dc7b37ff9ebcd0bdb857a7103061b6e138f5ed2b
Victor <victor.hugooo@hotmail.com.br>
2021-03-01 10:35:43 -0300

    Ajuste visual na listagem de artigos

--- src/Components/Visuais/artigos.js
+++ src/Components/Visuais/artigos.js
@@ -1,25 +1,70 @@
 import React from "react"
 import {FlatList, Image, Text, TouchableOpacity, View} from "react-native"
-import styles from "../../View/Glossario/styles"
 import TelaDeErro from "./telaDeErro";
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    listitem: {
+        backgroundColor: "#fff",
+        flexWrap: "wrap",
+        flexDirection: "row",
+        padding: 6,
+        marginHorizontal: 0,
+        marginVertical: 0
+
+    },
+    txtTitulo: {
+        fontSize: 16,
+        color: '#4f40b5',
+        fontWeight: 'bold'
+    },
+    txtDescricao: {
+        marginTop: 2,
+        color: '#000'
+    },
+    containerImagem: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignContent: "center",
+        justifyContent: "center",
+        backgroundColor: "#4f40b5",
+        flexWrap: "wrap",
+        marginTop: 10,
+        marginHorizontal: 16
+    }, 
+    listitemContainerDescricao: {
+        flex: 1,
+        flexGrow: 1,
+        alignItems: "flex-start",
+        justifyContent: "center",
+        marginVertical: 4,
+        borderRadius: 6,
+        paddingTop: 6,
+        paddingBottom: 12,
+        paddingEnd: 30,
+        borderBottomWidth: 0.03,
+        borderBottomColor: "#e4ddf3",
+    }
+})
 
 
-export default function artigos(dados) {
-    console.log(dados)
+export default function Artigos(dados) {
     if (dados.info.length < 1) {
         return <TelaDeErro mensagem={"Nenhum resultado encontrado!"}/>
     }
 
     //RenderItem da flatList
     function itemListModel(props) {
+        console.log(props)
         return (
             <TouchableOpacity
                 style={styles.listitem}
                 onPress={() => {}}>
                 <View style={styles.containerImagem}>
-                    <Image
-                        style={styles.imagem}
-                        source={require('../../../assets/iconeDoencas.png')}/>
+                    <Text style={{alignSelf: "center", color: "#fff", fontSize: 18}}>
+                        {props.name.charAt(0)}
+                    </Text>
                 </View>
                 <View style={styles.listitemContainerDescricao}>
                     <Text style={styles.txtTitulo}>{props.name}</Text>
